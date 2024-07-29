949e04cb5d532c65c3254057cb420f1e1f937831
VioletEvergardenZz <1561862432@qq.com>
2024-07-29 11:44:37 +0800

    数据库数据

--- app01/views.py
+++ app01/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from app01 import models
 
 # Create your views here.
+
+'部门列表'
 def depart_list(request):
-    return render(request,'depart_list.html')
+
+    # 去数据库中获取所有的部门列表
+    # [对象,对象,对象]
+    queryset = models.Department.objects.all()
+
+    return render(request,'depart_list.html',{'queryset':queryset})
