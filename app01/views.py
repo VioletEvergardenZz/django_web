418f5baa1cf58f6b24318bdf83f6d3396dd1ba3e
VioletEvergardenZz <1561862432@qq.com>
2024-07-29 21:30:02 +0800

    靓号管理-添加靓号的两种方式

--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from app01 import models
 from django import forms
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
@@ -128,7 +129,7 @@ def pretty_list(request):
     return render(request,'pretty_list.html',{'queryset':queryset})
 
 class PrettyNumModelForm(forms.ModelForm):
-
+    # 验证:方式1
     mobile = forms.CharField(
         label="手机号",
         validators=[RegexValidator(r'^1[3-9]\d{9}$','手机号格式错误')]
@@ -143,6 +144,13 @@ class PrettyNumModelForm(forms.ModelForm):
         # 循环找到所有的插件 添加了 class ='form-control'
         for name,field in self.fields.items():
             field.widget.attrs = {'class':'form-control','placeholder':field.label}
+
+    # 验证:方式二
+    # def clean_mobile(self):
+    #     txt_mobile = self.cleaned_data['mobile']
+    #     if len(str(txt_mobile)) != 11:
+    #         raise ValidationError("格式错误")
+    #     return txt_mobile
 
 '添加靓号'
 def pretty_add(request):
