e1e4d60b183870bfa97ae143c585a23504fd94d6
VioletEvergardenZz <1561862432@qq.com>
2024-07-29 12:12:18 +0800

    添加

--- app01/views.py
+++ app01/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from app01 import models
 
 # Create your views here.
@@ -12,4 +12,12 @@ def depart_list(request):
 
 '添加部门'
 def depart_add(request):
-    return render(request,'depart_add.html')
+    if request.method == "GET":
+        return render(request,'depart_add.html')
+
+    # 获取用户POST提交过来的数据
+    title = request.POST.get('title')
+    # 保存到数据库
+    models.Department.objects.create(title=title)
+    # 重定向回部门列表
+    return redirect('/depart/list/')
