1803b77d770d801718cb87d6834da3ee03ca694f
VioletEvergardenZz <1561862432@qq.com>
2024-07-29 10:43:08 +0800

    表结构的创建

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+'部门表'
+class Department(models.Model):
+    title = models.CharField(verbose_name='标题',max_length=32)
+
+'员工表'
+class UserInfo(models.Model):
+    name = models.CharField(verbose_name='姓名',max_length=16)
+    password = models.CharField(verbose_name='密码',max_length=64)
+    age = models.IntegerField(verbose_name='年龄')
+    account = models.DecimalField(verbose_name='账户余额',max_digits=10,decimal_places=2,default=0)
+    create_time = models.DateTimeField(verbose_name='入职时间')
+    # depart_id 有约束 级联删除
+    depart = models.ForeignKey(to="department",to_field="id",on_delete=models.CASCADE)
+    # Django中的约束
+    gender_choices = (
+        (1,"男"),
+        (2,"女"),
+    )
+    gender = models.SmallIntegerField(verbose_name="性别",choices=gender_choices)
