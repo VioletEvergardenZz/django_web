0862d103321a40ec8cc0502338d00af6b1d0c141
VioletEvergardenZz <1561862432@qq.com>
2024-07-31 19:27:18 +0800

    图片验证码(校验)

--- app01/views/account.py
+++ app01/views/account.py
@@ -31,6 +31,14 @@ def login(request):
         return render(request, 'login.html', {'form': form})
     form = LoginForm(data=request.POST)
     if form.is_valid():
+
+        # 验证码的校验
+        user_input_code = form.cleaned_data.pop('code')
+        code = request.session.get('image_code', "")
+        if code.upper() != user_input_code.upper():
+            form.add_error("code", "验证码错误")
+            return render(request, 'login.html', {'form': form})
+
         # 去数据库校验用户名和密码是否正确，获取用户对象、None
         admin_object = models.Admin.objects.filter(**form.cleaned_data).first()
         if not admin_object:
@@ -40,6 +48,8 @@ def login(request):
         # 用户名和密码正确
         # 网站生成随机字符串; 写到用户浏览器的cookie中；在写入到session中；
         request.session["info"] = {'id': admin_object.id, 'name': admin_object.username}
+        # session可以保存7天
+        request.session.set_expiry(60 * 60 * 24 * 7)
         return redirect("/admin/list/")
 
     return render(request, 'login.html', {'form': form})
@@ -48,6 +58,12 @@ def login(request):
 def image_code(request):
     # 调佣pillow函数
     img,code_string = check_code()
+
+    # 写入到自己的sessin中(以便于后续获取验证码在进行校验)
+    request.session["image_code"] = code_string
+    # 给session设置60s超时
+    request.session.set_expiry(60)
+
     stream = BytesIO()
     img.save(stream, 'png')
     return HttpResponse(stream.getvalue())
