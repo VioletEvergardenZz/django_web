6b20d90980ce610698d048f06b132d55bdd76bfa
VioletEvergardenZz <1561862432@qq.com>
2024-07-31 11:15:38 +0800

    管理员-删除和重置密码

--- app01/views/admin.py
+++ app01/views/admin.py
@@ -49,6 +49,31 @@ class AdminEditModelForm(BootStrapModelForm):
         model = models.Admin
         fields = ['username']
 
+class AdminRestModelForm(BootStrapModelForm):
+    confirm_password = forms.CharField(
+        label="确认密码",
+        widget=forms.PasswordInput(render_value=True)
+       )
+    class Meta:
+        model = models.Admin
+        fields = ['password','confirm_password']
+        widgets ={
+            'password': forms.PasswordInput(render_value=True),
+        }
+    def clean_password(self):
+        pwd = self.cleaned_data.get('password')
+        md5_pwd = md5(pwd)
+        # 去数据库校验当前密码和数据库密码是否一致
+        exits = models.Admin.objects.filter(id = self.instance.pk,password=md5_pwd).exists()
+        if exits:
+            raise ValidationError('密码不能与之前的一致')
+        return md5(pwd)
+    def clean_confirm_password(self):
+        pwd = self.cleaned_data.get('password')
+        confirm = md5(self.cleaned_data.get('confirm_password'))
+        if confirm != pwd:
+            raise ValidationError("密码不一致,请重新输入")
+        return confirm
 
 '添加管理员'
 def admin_add(request):
@@ -72,6 +97,26 @@ def admin_edit(request,nid):
         form = AdminEditModelForm(instance=row_object)
         return render(request,'change.html',{'form':form,'title':title})
     form = AdminEditModelForm(data=request.POST,instance=row_object)
+    if form.is_valid():
+        form.save()
+        return redirect('/admin/list/')
+    return render(request,'change.html',{'form':form,'title':title})
+
+'删除管理员'
+def admin_delete(request,nid):
+    models.Admin.objects.filter(id=nid).delete()
+    return redirect('/admin/list/')
+
+'重置密码'
+def admin_reset(request,nid):
+    row_object = models.Admin.objects.filter(id=nid).first()
+    if not row_object:
+        return redirect('/admin/list/')
+    title = "重置密码 - {}".format(row_object.username)
+    if request.method == 'GET':
+        form = AdminRestModelForm()
+        return render(request,'change.html',{'form':form,'title':title})
+    form = AdminRestModelForm(data=request.POST,instance=row_object)
     if form.is_valid():
         form.save()
         return redirect('/admin/list/')
