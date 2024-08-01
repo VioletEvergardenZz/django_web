eeec728f22885010761fe1eb86aee1fe7ecbba8e
VioletEvergardenZz <1561862432@qq.com>
2024-08-01 11:43:16 +0800

    订单功能

--- app01/views/order.py
+++ app01/views/order.py
@@ -30,3 +30,79 @@ def order_list(request):
 
     return render(request, 'order_list.html', context)
 
+
+'新建订单（Ajax请求)'
+@csrf_exempt
+def order_add(request):
+    form = OrderModelForm(data=request.POST)
+    if form.is_valid():
+        # 订单号：额外增加一些不是用户输入的值（自己计算出来）
+        form.instance.oid = datetime.now().strftime("%Y%m%d%H%M%S") + str(random.randint(1000, 9999))
+
+        # 固定设置管理员ID
+        form.instance.admin_id = request.session["info"]["id"]
+
+        # 保存到数据库中
+        form.save()
+        return JsonResponse({"status": True})
+    return JsonResponse({"status": False, 'error': form.errors})
+
+'删除订单'
+def order_delete(request):
+    uid = request.GET.get('uid')
+    exists = models.Order.objects.filter(id=uid).exists()
+    if not exists:
+        return JsonResponse({"status": False, 'error': "删除失败，数据不存在。"})
+
+    models.Order.objects.filter(id=uid).delete()
+    return JsonResponse({"status": True})
+
+'根据ID获取订单详细'
+def order_detail(request):
+    # 方式1
+    """
+    uid = request.GET.get("uid")
+    row_object = models.Order.objects.filter(id=uid).first()
+    if not row_object:
+        return JsonResponse({"status": False, 'error': "数据不存在。"})
+
+    # 从数据库中获取到一个对象 row_object
+    result = {
+        "status": True,
+        "data": {
+            "title": row_object.title,
+            "price": row_object.price,
+            "status": row_object.status,
+        }
+    }
+    return JsonResponse(result)
+    """
+
+    # 方式2
+    uid = request.GET.get("uid")
+    row_dict = models.Order.objects.filter(id=uid).values("title", 'price', 'status').first()
+    if not row_dict:
+        return JsonResponse({"status": False, 'error': "数据不存在。"})
+
+    # 从数据库中获取到一个对象 row_object
+    result = {
+        "status": True,
+        "data": row_dict
+    }
+    return JsonResponse(result)
+
+
+'编辑订单'
+@csrf_exempt
+def order_edit(request):
+    uid = request.GET.get("uid")
+    row_object = models.Order.objects.filter(id=uid).first()
+    if not row_object:
+        return JsonResponse({"status": False, 'tips': "数据不存在，请刷新重试。"})
+
+    form = OrderModelForm(data=request.POST, instance=row_object)
+    if form.is_valid():
+        form.save()
+        return JsonResponse({"status": True})
+
+    return JsonResponse({"status": False, 'error': form.errors})
